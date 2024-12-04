50e893b7703aa382c1641d8b800fe911dfa3056f
popoyu521 <153209648+popoyu521@users.noreply.github.com>
2024-12-04 23:52:11 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,7 +909,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://voip77.x3322.net:21114".to_owned()
+    "http://voip77.x3322.net:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
